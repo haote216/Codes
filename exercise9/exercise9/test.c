06a78a36b81c26b1afd81f59c953eb89961f5616
haote <1419367121@qq.com>
2018-12-03 22:33:30 +0800

    练习9

--- exercise9/exercise9/test.c
+++ exercise9/exercise9/test.c
@@ -3,8 +3,28 @@
 #include<windows.h>
 int main()
 {
-	int a = 1;
-	printf("%d\n", sizeof int);
+	struct Stu{
+		char name[20];
+		int age;
+		char sex[5];
+		double score;
+	};
+	struct Stu s;
+	struct Stu *ps = &s;
+	s.name = "name";
+	s.score = 5.24;
+
+	//int i = 0, a = 0, b = 2, c = 3, d = 4;
+	////i = a++ && ++b && d++;
+	//i = a++||++b||d++;
+	//printf("a = %d\n b = %d\n c = %d\nd = %d\n", a, b, c, d);
+
+
+	/*int i = 5;
+	int ret = (--i) - (--i)-(--i)-(--i);
+	printf("%d\n", ret);
+	printf("%d\n", i);*/
+	//printf("%d\n", sizeof int);
 	system("pause");
 	return 0;
 }
