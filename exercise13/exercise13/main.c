425654778c594acc01cf3c83b1f710653ba5ee72
haote <1419367121@qq.com>
2019-01-21 20:53:28 +0800

    练习13

---
+++ exercise13/exercise13/main.c
@@ -0,0 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+	//int a = /*/*/0*/**/ 1;
+	//printf("%d\n", a);
+	int a = 1;
+	int b = 1;
+	printf("%d\n", a++++ +b);
+
+
+	system("pause");
+	return 0;
+}
