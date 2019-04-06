6334710ce41f856143f9d94f0b8fc391b4c1c89c
haote216 <1419367121@qq.com>
2019-04-06 22:16:44 +0800

    C# exercise1

---
+++ 1/1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            byte b = 24;
+            Console.WriteLine("a={0}", a);
+            Console.WriteLine("b={0}", b);
+            Console.ReadLine();
+            
+        }
+    }
+}
