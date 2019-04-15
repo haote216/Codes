8206e04208f7bb7d8b0b64ac691c0b6dcca441e9
haote216 <1419367121@qq.com>
2019-04-15 22:55:44 +0800

    Simulated implementation of vector

---
+++ vector 1/vector 1/Vector.h
@@ -0,0 +1,148 @@
+#pragma once
+
+#include<assert.h>
+
+namespace bit
+{
+	template<class T>
+	class Vector
+	{
+
+	public:
+		typedef T* iterator;
+		//typedef const T* const_iterator;
+
+
+		//放在类里面，成为内联
+		iterator begin()
+		{
+			return _start;
+		}
+	
+		iterator end()
+		{
+			return _finish;
+		}
+
+		const iterator cbegin()const
+		{
+			return _start;
+		}
+
+		const iterator cend()const 
+		{
+			return _finish;
+		}
+		size_t Size()const
+		{
+			return _finish - _start;
+		}
+
+		size_t Capacity()const
+		{
+			return _endofstorage - _start;
+		}
+
+		T& operator[](size_t pos)
+		{
+			assert(pos < Size());
+			return _start[pos];
+		}
+
+		const T& operator[](size_t pos)const
+		{
+			assert(pos < Size());
+			return _start[pos];
+		}
+		Vector()
+			:_start(nullptr)
+			, _finish(nullptr)
+			, _endofstorage(nullptr)
+		{}
+
+		//Vector<int> v2(v1);
+		Vector(const Vector<T>& v);
+		Vector<T>& operator=(Vector<T> v);
+
+		~Vector()
+		{
+			if (_start)
+			{
+				delete[] _start;
+				_start = _finish = _endofstorage = nullptr;
+			}
+		}
+
+		void Reserve(size_t n)
+		{
+			size_t size = Size();      //这里的Size（）是有效元素的大小，后面会释放掉空间，然后再使用会导致出错，所以先求出来进行保存后面再使用。
+			if (n > Capacity())
+			{
+				T* tmp = new T[n];
+				if (_start)
+				{
+					memcpy(tmp, _start, sizeof(T)*Size());
+					delete[] _start;
+				}
+			
+				_start = tmp;
+				_finish = _start + size ;   
+				_endofstorage = _start + n;
+
+				
+			}
+		}
+		void Resize(size_t n, const T& val = T());
+		void PushBack(const T& x)
+		{
+			Insert(_finish, x);
+		}
+		void PopBack(const T& x);
+		//Insert会引发迭代器失效问题，所以为了能够继续使用迭代器，使用偏移量计算出pos指向的位置，如果想*或者其他使用迭代器，
+		//  就得传参传引用，最后++pos，因为pos指的3的位置，插入后pos指的是插入数据的位置
+		void Insert(iterator pos, const T& x)
+		{
+			assert(pos <= _finish);
+			size_t offset = pos - _start;
+			if (_finish == _endofstorage)
+			{
+				size_t newcapacity = Capacity() == 0 ? 2 : Capacity() * 2;
+				Reserve(newcapacity);
+				pos = _start + offset;
+			}
+			iterator end = _finish;
+			while (end > pos)
+			{
+				*end = *(end - 1);
+				--end;
+			}
+			*pos = x;
+			//++pos;    //为什么？
+			++_finish;
+		}
+
+		iterator Erase(iterator pos)
+		{
+			assert(pos < _finish);
+			iterator begin = pos;
+			while (begin < _finish - 1)
+			{
+				*begin = *(begin + 1);
+				++begin;
+			}
+			--_finish;
+			return pos;
+
+		}
+
+	private:
+		//T* _array;
+		//size_t _size;
+		//size_t _capacity;
+
+		T* _start;
+		T* _finish;
+		T* _endofstorage;
+	};
+
+}
---
+++ vector 1/vector 1/main.cpp
@@ -0,0 +1,172 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+#include<string>
+#include"Vector.h"
+using namespace std;
+
+//void test_vector1()
+//{
+//	string str("hello");
+//
+//	vector<int> v1;
+//	vector<int> v2(10, 3);
+//	vector<int> v3(v2.begin(), v2.end());
+//	vector<int> v4(str.begin(), str.end());
+//
+//	//迭代器
+//	vector<int>::iterator it = v2.begin();
+//	while (it != v2.end())
+//	{
+//		cout << *it << " ";
+//		++it;
+//	}
+//	cout << endl;
+//	
+//	//反向迭代器
+//	vector<int>::reverse_iterator rit = v4.rbegin();
+//	while (rit != v4.rend())
+//	{
+//		cout << *rit << " ";
+//		++rit;
+//	}
+//	cout << endl;
+//
+//	//const迭代器
+//	vector<int>::const_iterator cit = v4.cbegin();
+//	while (cit != v4.cend())
+//	{
+//		cout << *cit << " ";
+//		++cit;
+//	}
+//	cout << endl;
+//
+//	// operator[]
+//	for (size_t i = 0; i < v4.size(); ++i)
+//	{
+//		cout << v4[i] << " ";
+//	}
+//	cout << endl;
+//
+//	//语法糖
+//	for (const auto& e : v4)
+//	{
+//		cout << e << " ";
+//	}
+//	cout << endl;
+//
+//	vector<string> strv;
+//	strv.push_back("hello");
+//	strv.push_back("world");
+//	for (const auto& s : strv)
+//	{
+//		cout << s << " ";
+//	}
+//	cout << endl;
+//
+//	//遍历
+//	//1.迭代器
+//	//2.for + [i]
+//	//3.for-each  C++11
+//}
+
+//void test_vector2()
+//{
+//	size_t sz;
+//	std::vector<int> foo;
+//	sz = foo.capacity();
+//	cout << "making foo grow: \n";
+//	foo.resize(100);      //改变size
+//	//foo.reserve(100);   //是把容量capacity开辟到100了
+//	for (int i = 0; i < 100; ++i)
+//	{
+//		//foo.push_back(i);         //进行尾插
+//		foo[i] = i;                 //直接进行访问，不能和resever搭配使用
+//		if (sz != foo.capacity()){
+//			sz = foo.capacity();
+//			cout << "capacity changed: " << sz << endl;
+//		}
+//	}
+//	for (const auto& e : foo)
+//	{
+//		cout << e << " ";
+//	}
+//	cout << endl;
+//}
+
+//void test_vector3()
+//{
+//	vector<int> v;
+//	v.push_back(1);
+//	v.push_back(2);
+//	v.push_back(3);
+//	v.push_back(4);
+//
+//	//vector<int>::iterator pos = find(v.begin(), v.end(), 2);
+//	auto pos = find(v.begin(), v.end(), 3);
+//	//if (pos != v.end())
+//	//{
+//	//	v.insert(pos, 20);   //insert是在pos之前插
+//	//}
+//
+//	//迭代器失效     insert增容会引发迭代器失效,增容后pos还指向原来的空间,而原来的空间已经释放,像野指针。
+//	v.erase(pos);
+//
+//	//迭代器失效     erase删除,再对迭代器进行访问，就会出错。
+//
+//	for (auto e : v)
+//	{
+//		cout << e << " ";
+//	}
+//	cout << endl;
+//}
+
+
+void TestVector1()
+{
+	bit::Vector<int> v1;
+	v1.PushBack(1);
+	v1.PushBack(2);
+	v1.PushBack(3);
+	v1.PushBack(4);
+	v1.PushBack(5);
+	v1.Insert(v1.begin(), 30);
+	for (const auto& e : v1)
+	{
+		cout << e << endl;
+	}
+	cout << endl;
+
+	bit::Vector<int>::iterator it = v1.begin();
+	while (it != v1.end())
+	{
+		cout << *it << " ";
+		++it;
+	}
+	cout<< endl;
+
+	for (size_t i = 0; i < v1.Size(); ++i)
+	{
+		cout << v1[i] << " ";
+	}
+	cout << endl;
+
+
+}
+
+//void TestVector2()
+//{
+//	bit::Vector<int> v1;
+//	v1.PushBack(1);
+//	v1.PushBack(2);
+//	v1.PushBack(3);
+//	v1.PushBack(4);
+//	v1.PushBack(5);
+//
+//}
+int main()
+{
+	TestVector1();
+	system("pause");
+	return 0;
+}
