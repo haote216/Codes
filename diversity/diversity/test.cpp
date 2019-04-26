fb15597b0734fece268eeec8fbca0ab24f4f1f06
haote216 <1419367121@qq.com>
2019-04-26 20:20:30 +0800

    update two

--- diversity/diversity/test.cpp
+++ diversity/diversity/test.cpp
@@ -1,46 +1,46 @@
 #include<iostream>
 
-class Person{
-public:
-	virtual void BuyTicket()
-	{
-		std::cout << "买票--全价" << std::endl;
-	}
-	virtual ~Person()
-	{
-		std::cout << "~Person " << std::endl;
-	}
-};
-
-class Student : public Person{
-public:
-	virtual void BuyTicket()
-	{
-		std::cout << "买票--半价" << std::endl;
-	}
-
-	virtual ~Student()
-	{
-		std::cout << "~Student " ;
-	}
-};
-
-void Func(Person & p)
-{
-	p.BuyTicket();
-}
-
-int main()
-{
-	Person p;
-	Student s;
-	Func(p);
-	Func(s);
-
-
-	system("pause");
-	return 0;
-}
+//class Person{
+//public:
+//	virtual void BuyTicket()
+//	{
+//		std::cout << "买票--全价" << std::endl;
+//	}
+//	virtual ~Person()
+//	{
+//		std::cout << "~Person " << std::endl;
+//	}
+//};
+//
+//class Student : public Person{
+//public:
+//	virtual void BuyTicket()
+//	{
+//		std::cout << "买票--半价" << std::endl;
+//	}
+//
+//	virtual ~Student()
+//	{
+//		std::cout << "~Student " ;
+//	}
+//};
+//
+//void Func(Person & p)
+//{
+//	p.BuyTicket();
+//}
+//
+//int main()
+//{
+//	Person p;
+//	Student s;
+//	Func(p);
+//	Func(s);
+//
+//
+//	system("pause");
+//	return 0;
+//}
 
 
 
@@ -119,3 +119,49 @@ int main()
 //	return 0;
 //}
 //
+
+class Base1 {
+public:
+	virtual void func1() { std::cout << "Base1::func1" << std::endl; }
+	virtual void func2() { std::cout << "Base1::func2" << std::endl; }
+private:
+	int b1;
+};
+class Base2 {
+public:
+	virtual void func1() { std::cout << "Base2::func1" << std::endl; }
+	virtual void func2() { std::cout << "Base2::func2" << std::endl; }
+private:
+	int b2;
+};
+class Derive : public Base1, public Base2 {
+public:
+	virtual void func1() { std::cout << "Derive::func1" << std::endl; }
+	virtual void func3() { std::cout << "Derive::func3" << std::endl; }
+private:
+	int d1;
+};
+typedef void(*VFPTR) ();
+void PrintVTable(VFPTR vTable[])
+{
+	std::cout << " 虚表地址>" << vTable << std::endl;
+	for (int i = 0; vTable[i] != nullptr; ++i)
+	{
+		printf(" 第%d个虚函数地址 :0X%x,->", i, vTable[i]);
+		VFPTR f = vTable[i];
+		f();
+	}
+	std::cout << std::endl;
+}
+int main()
+{
+	Derive d;
+
+	VFPTR* vTableb1 = (VFPTR*)(*(int*)&d);
+	PrintVTable(vTableb1);
+
+	VFPTR* vTableb2 = (VFPTR*)(*(int*)((char*)&d + sizeof(Base1)));
+	PrintVTable(vTableb2);
+	system("pause");
+	return 0;
+}
