cb64f1bdf29b35574a0725da583a46fb4133e187
haote216 <1419367121@qq.com>
2019-05-19 22:13:48 +0800

    C++ Primer exercise

---
+++ exercise19/exercise19/test.cpp
@@ -0,0 +1,95 @@
+#include<iostream>
+#include<stdio.h>
+#include<windows.h>
+#include<string>
+#include<vector>
+
+using namespace std;
+//int main()
+//{
+//	string s0;
+//	string s1 = "hello";
+//	//string s2 = "world";
+//	//string s3 = s1 + ",";
+//	////string s4 = "hello" + ",";
+//	//string s5 = s1 + "," + "world";
+//	////string s6 = "hello" + "," + s2;
+//	//while (getline(cin, s1))
+//	//{
+//	//	cout << s1 << endl;
+//	//}
+//
+//	//string::size_type num = s.size();
+//	//printf("%d\n",num);
+//	cout << s0[0] << endl;
+//	system("pause");
+//	return 0;
+//}
+
+//int main()
+//{
+//	string s("hello world!!!!!");
+//	string::size_type punct_cnt = 0;
+//	for (string::size_type index = 0; index != s.size(); ++index)
+//	{
+//		if (ispunct(s[index]))
+//			++punct_cnt;
+//		
+//	}
+//	cout << punct_cnt << " " << "punctuation characters in" << s << endl;
+//	system("pause");
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	string s1;
+//	string s2;
+//	while (1)
+//	{
+//		cin >> s2;
+//		s1 = s1 + " " + s2;
+//		cout << s1 << endl;
+//	}
+//	system("pause");
+//	return 0;
+//}
+
+
+
+//编一个程序，从一个string对象中去除标点符号
+//int main()
+//{
+//	string s,m;
+//	cin >> s;
+//	for (string::size_type index = 0; index != s.size(); ++index)
+//	{
+//		if (!ispunct(s[index]))
+//			m += s[index];		
+//	}
+//	cout << m << endl;
+//	system("pause");
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	//vector<vector<int>> s1;
+//	//vector<string> s2 = s1;
+//	//vector<string> s2(10, "null");
+//	string s;
+//	vector<int>s1;
+//	vector<int>s2(10);
+//	vector<int>s3(10, 42);
+//
+//	vector<string> s4;
+//	vector<string> s5(10);
+//	vector<string> s6(10, "a");
+//
+//	cout << s[0] << endl;
+//	system("pause");
+//	return 0;
+//}
+
