2f87a3f3140de185119e75d6df77a3632035d893
haote216 <1419367121@qq.com>
2019-05-25 23:50:07 +0800

    exercise25

---
+++ exercise25/exercise25/test.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<string>
+#include<vector>
+using namespace std;
+//int main()
+//{
+//	int x = 1;
+//	do{
+//		printf("%2d\n", x++);
+//	} while (x--);
+//	system("pause");
+//	return 0;
+//}
